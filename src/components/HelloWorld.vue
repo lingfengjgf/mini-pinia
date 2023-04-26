ab66ab3c094812ee9f05b0bdd53b66fba4bec715
lingfengjgf <lingfengjgf@163.com>
2023-04-26 22:56:47 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script setup>
+// import { ref } from 'vue'
+
+// defineProps({
+//   msg: String,
+// })
+
+// const count = ref(0)
+
+import {useCounterStore} from '../store/counter'
+const counter = useCounterStore();
+</script>
+
+<template>
+  <div>{{ counter.count }} - {{ counter.doubleCounter }}</div>
+  <div @click="counter.count++">counter.count++</div>
+  <div @click="counter.inc()">counter.inc()</div>
+  <div @click="counter.$patch({count:counter.count+1})">counter.$patch({count:counter.count+1})</div>
+  <div @click="counter.$patch((state)=>state.count++)">counter.$patch((state)=>state.count++)</div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
